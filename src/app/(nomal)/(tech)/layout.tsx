8850312a5983a541d39420fab0fc07a9d148148e
moat1 <108268542+moat1@users.noreply.github.com>
2023-06-26 01:55:53 +0900

    🔥 コンポーネント化により、不要なコードを削除

--- src/app/(nomal)/(tech)/layout.tsx
+++ src/app/(nomal)/(tech)/layout.tsx
@@ -1,60 +1,12 @@
-"use client";
-import CircleIcon from "@mui/icons-material/Circle";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import TechNav from "@/components/techNav";
+import TechTitle from "@/components/techTitle";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const TECH_NAME = [
-    { href: "/tech-introduction", title: "Introduction" },
-    { href: "/html-page", title: "HTML5" },
-    { href: "/css-page", title: "CSS3" },
-    { href: "/javascript-page", title: "JavaScript(ES6)" },
-    { href: "/typescript-page", title: "TypeScript" },
-    { href: "/react-page", title: "React" },
-    { href: "/nextjs-page", title: "Next.js" },
-    { href: "/tailwindcss-page", title: "TailwindCSS" },
-    { href: "/sass-page", title: "Sass" },
-    { href: "/cssmodules-page", title: "CSS Modules" },
-    { href: "/vscode-page", title: "VSCode" },
-    { href: "/github-page", title: "GitHub" },
-    { href: "/npmyarn-page", title: "npm/yarn" },
-    { href: "/vercel-page", title: "Vercel" },
-    { href: "/slack-page", title: "Slack" },
-  ];
-
-  const pathname = usePathname();
-
-  const title = TECH_NAME.find((data) => data.href.includes(pathname))?.title;
-
   return (
     <div className="flex w-full gap-5 p-16 max-sm:flex-col max-sm:px-5 max-sm:py-12">
-      <div className="flex w-56 rounded-3xl bg-gray-100 px-10 py-10 max-sm:h-[250px] max-sm:w-full max-sm:overflow-auto">
-        <ul className="w-full">
-          <h1 className="m-3 flex justify-center text-xl font-bold">Navigation</h1>
-          <p className="flex justify-center text-sm">下にスクロールできます</p>
-          {TECH_NAME.map((data) => {
-            return (
-              <li
-                key={data.href}
-                className="border-b-[1px] py-3 text-gray-600 hover:text-blue-500"
-              >
-                <Link href={data.href}>
-                  {title === data.title ? (
-                    <span className="font-bold text-black">
-                      {<CircleIcon className="mr-2 h-5 w-5" />}
-                      {data.title}
-                    </span>
-                  ) : (
-                    <span>{data.title}</span>
-                  )}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <TechNav />
       <div className="w-full px-10 py-5">
-        <h1 className="flex justify-center p-5 text-2xl font-bold">{title}</h1>
+        <TechTitle />
         {children}
       </div>
     </div>
--- src/app/(nomal)/account/page.tsx
+++ src/app/(nomal)/account/page.tsx
@@ -1,27 +1,8 @@
 "use client";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import Link from "next/link";
+
+import AccountCard from "@/components/accountCard";
 
 export default function Account() {
-  const ACCOUNT = [
-    {
-      href: "https://twitter.com/Km7G0svt4Jlvwxm",
-      bgclass:
-        "flex h-[200px] content-center justify-center w-full rounded-tl-xl rounded-tr-xl border-b-[1px] bg-gradient-to-b from-sky-500 to-sky-200",
-      icon: <TwitterIcon className="m-auto h-20 w-20 text-white" />,
-      title: "Twitter",
-      description: "私が所有しているアカウントの一覧をまとめています。",
-    },
-    {
-      href: "https://github.com/moat1",
-      bgclass:
-        "flex h-[200px] content-center justify-center rounded-tl-xl rounded-tr-xl border-b-[1px] bg-gradient-to-b from-black to-gray-500",
-      icon: <GitHubIcon className="m-auto h-20 w-20 text-white" />,
-      title: "GitHub",
-      description: "私が所有しているアカウントの一覧をまとめています。",
-    },
-  ];
   return (
     <main className="m-auto text-gray-800">
       <div className="px-20 max-sm:px-5">
@@ -33,25 +14,7 @@ export default function Account() {
           <p>下のカードをクリックするとアカウントのリンクへ飛ぶことが出来ます。</p>
         </div>
       </div>
-
-      <div className="my-20 flex flex-wrap justify-center gap-10">
-        {ACCOUNT.map((data) => {
-          return (
-            <div
-              key={data.href}
-              className="w-72 rounded-xl border bg-white shadow-xl hover:border-gray-400 hover:shadow-2xl"
-            >
-              <Link href={data.href}>
-                <div className={data.bgclass}>{data.icon}</div>
-                <div className="p-2">
-                  <h1 className="flex justify-center p-2 font-bold">{data.title}</h1>
-                  <div className="px-2 text-sm">{data.description}</div>
-                </div>
-              </Link>
-            </div>
-          );
-        })}
-      </div>
+      <AccountCard />
     </main>
   );
 }
