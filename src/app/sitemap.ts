7ba12e7863ecbe31756047770002b8b4e6b5a188
moat1 <108268542+moat1@users.noreply.github.com>
2023-07-05 16:06:59 +0900

    🔍 sitemapの追加

---
+++ src/app/sitemap.ts
@@ -0,0 +1,50 @@
+import { MetadataRoute } from "next";
+
+import { getContents } from "@/libs/client";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const data = await getContents();
+  const contents = data.contents;
+
+  const domain = process.env.SITE_DOMAIN;
+  const lastModified = new Date();
+
+  const staticPaths = [
+    {
+      url: `${domain}.com`,
+      lastModified,
+    },
+    {
+      url: `${domain}/tech-introduction`,
+      lastModified,
+    },
+    {
+      url: `${domain}/about`,
+      lastModified,
+    },
+    {
+      url: `${domain}/blog`,
+      lastModified,
+    },
+    {
+      url: `${domain}/account`,
+      lastModified,
+    },
+    {
+      url: `${domain}/privacypolicy`,
+      lastModified,
+    },
+    {
+      url: `${domain}/code`,
+      lastModified,
+    },
+  ];
+
+  const dynamicPaths = contents.map((item) => {
+    return {
+      url: `${domain}/${item.id}`,
+      lastModified,
+    };
+  });
+  return [...staticPaths, ...dynamicPaths];
+}
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly MICROCMS_API_KEY: string;
     readonly MICROCMS_SERVICE_DOMAIN: string;
+    readonly SITE_DOMAIN: string;
   }
 }
