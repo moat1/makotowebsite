a4d22759972d3e14580bd24cf68ee8cb02d176a9
moat1 <108268542+moat1@users.noreply.github.com>
2023-07-06 18:01:01 +0900

    🐛 トップページのmetaデータ場所を変更

--- src/app/(nomal)/layout.tsx
+++ src/app/(nomal)/layout.tsx
@@ -2,6 +2,31 @@ import Footer from "@/components/footer";
 
 import Header from "../../components/header";
 
+const siteName = "Makoto Website";
+const description =
+  "私は文系学部に在籍している大学生で、独学でWebアプリケーション開発（主にフロントエンド）を行っています。";
+const url = process.env.SITE_DOMAIN;
+
+export const metadata = {
+  title: siteName,
+  description,
+  openGraph: {
+    title: siteName,
+    description,
+    url,
+    siteName,
+    locale: "ja_JP",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteName,
+    description,
+    site: "@MakotoScript",
+    creator: "@MakotoScript",
+  },
+};
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,31 +4,6 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const siteName = "Makoto Website";
-const description =
-  "私は文系学部に在籍している大学生で、独学でWebアプリケーション開発（主にフロントエンド）を行っています。";
-const url = process.env.SITE_DOMAIN;
-
-export const metadata = {
-  title: siteName,
-  description,
-  openGraph: {
-    title: siteName,
-    description,
-    url,
-    siteName,
-    locale: "ja_JP",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteName,
-    description,
-    site: "@MakotoScript",
-    creator: "@MakotoScript",
-  },
-};
-
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ja" className="text-nomal-color">
